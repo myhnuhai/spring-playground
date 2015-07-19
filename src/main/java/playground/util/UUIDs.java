85ad82838ec36ebbf388883658684fdcb4c88c07
Bill Ying <yingzhor@gmail.com>
2015-07-19 13:04:08 +0800

    update pom.xml

--- src/main/java/playground/util/UUIDs.java
+++ src/main/java/playground/util/UUIDs.java
@@ -1,18 +1,21 @@
 package playground.util;
 
-import java.util.Locale;
+import java.util.Iterator;
 import java.util.UUID;
+import java.util.stream.Stream;
 
-public class UUIDs {
+public final class UUIDs {
 
     private UUIDs() {
         super();
     }
 
+    private static Iterator<String> UUID_ITERATOR = Stream.generate(() -> UUID.randomUUID().toString()).iterator();
+
     /* -------------------------------------------------------- */
 
     public static String random() {
-        return UUID.randomUUID().toString().replaceAll("-", "").toLowerCase(Locale.ENGLISH);
+        return UUID_ITERATOR.next().replaceAll("-", "").toLowerCase();
     }
 
 }
