90b74b8263aedd3fd8693b442ee5d07ac67c07c8
Bill Ying <yingzhor@gmail.com>
2015-07-22 09:14:22 +0800

    添加单元测试Example

---
+++ src/test/java/test/TestCasesExample.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.hamcrest.Matchers;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.OutputCapture;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import playground.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class TestCasesExample {
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
+    @BeforeClass
+    public static void beforeClass() {
+
+    }
+
+    @Before
+    public void before() {
+
+    }
+
+    @Test
+    public void testExample() {
+        System.out.println("Hello World.");
+        Assert.assertThat(outputCapture.toString(), Matchers.containsString("Hello"));
+    }
+
+    @After
+    public void after() {
+
+    }
+
+    @AfterClass
+    public static void afterClass() {
+
+    }
+}
