a4094758dbcc240dba7827570204d31630ed7abb
Bill Ying <yingzhor@gmail.com>
2015-07-28 06:14:59 +0800

    update htmls add css.

--- src/main/java/playground/web/filter/LoggingFilter.java
+++ src/main/java/playground/web/filter/LoggingFilter.java
@@ -1,5 +1,6 @@
 package playground.web.filter;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -9,38 +10,21 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 
 public class LoggingFilter extends OncePerRequestFilter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingFilter.class);
 
-    private Set<String> uriExcludes = new HashSet<>();
-
-    public LoggingFilter() {
-        uriExcludes.add("/");
-        uriExcludes.add("/favicon.ico");
-    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String uri = request.getRequestURI();
 
-        if (!uriExcludes.contains(uri)) {
+        if (!StringUtils.endsWithAny(uri, ".css", ".js", ".ico")) {
             LOGGER.debug("uri: '{}'", uri);
         }
 
         filterChain.doFilter(request, response);
     }
 
-    public void setUriExcludes(List<String> uriExcludes) {
-        if (uriExcludes == null || uriExcludes.isEmpty()) {
-            this.uriExcludes.clear();
-        } else {
-            this.uriExcludes = new HashSet<>(uriExcludes);
-        }
-    }
 }
