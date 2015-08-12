e61845136e42980093522a75a8e6ca637e5f6d5f
Bill Ying <yingzhor@gmail.com>
2015-08-12 16:06:55 +0800

    update

--- src/main/java/playground/Application.java
+++ src/main/java/playground/Application.java
@@ -6,13 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
@@ -35,20 +37,24 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @PostConstruct
-    public void init() {
-        List<String> profiles = Arrays.asList(env.getActiveProfiles());
-        LOGGER.info("===========================================================================");
-        LOGGER.info("This log is very important !!!");
-        LOGGER.info("Unexpected bad things will happen if you are using wrong profiles.");
-        LOGGER.info("---------------------------------------------------------------------------");
-        if (profiles.isEmpty()) {
-            LOGGER.warn("No active profiles !");
-        } else {
-            LOGGER.info("Active profiles: ");
-            profiles.forEach(LOGGER::info);
-        }
-        LOGGER.info("===========================================================================");
+    @Bean
+    public ApplicationListener<ContextRefreshedEvent> applicationEventListener() {
+        return new ApplicationListener<ContextRefreshedEvent>() {
+            @Override
+            public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+                List<String> profiles = Arrays.asList(env.getActiveProfiles());
+                LOGGER.info("===========================================================================");
+                LOGGER.info("This log is very important !!!");
+                LOGGER.info("Unexpected bad things will happen if you are using wrong profiles.");
+                LOGGER.info("---------------------------------------------------------------------------");
+                if (profiles.isEmpty()) {
+                    LOGGER.warn("No active profiles !");
+                } else {
+                    LOGGER.info("Active profiles: ");
+                    profiles.forEach(LOGGER::info);
+                }
+                LOGGER.info("===========================================================================");
+            }
+        };
     }
-
 }
