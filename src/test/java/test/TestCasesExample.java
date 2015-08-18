9178ce9dc197998ba485d8ec7b5e6f5fc4674eca
Bill Ying <yingzhor@gmail.com>
2015-08-18 11:27:36 +0800

    update test code example

--- src/test/java/test/TestCasesExample.java
+++ src/test/java/test/TestCasesExample.java
@@ -6,10 +6,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.OutputCapture;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import playground.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
 public class TestCasesExample {
 
     @Rule
